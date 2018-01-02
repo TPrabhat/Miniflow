ccba0246709e81437cf79e26aa919025963ff2e7
Prabhat Telidevara <prabhat.tp@gmail.com>
2018-01-01 21:09:46 -0800

    Added a mean squared error sub class to calculate cost function

--- Miniflow.py
+++ Miniflow.py
@@ -88,7 +88,6 @@ class Linear(Node):
         """
         self.value = np.dot(self.inbound_nodes[0].value, self.inbound_nodes[1].value) + self.inbound_nodes[2].value
 
-
 class Sigmoid(Node):
     """
     You need to fix the `_sigmoid` and `forward` methods.
@@ -115,6 +114,32 @@ class Sigmoid(Node):
         # if you test without changing this method.
         self.value = self._sigmoid(self.inbound_nodes[0].value)
 
+
+class MSE(Node):
+    def __init__(self, y, a):
+        """
+        The mean squared error cost function.
+        Should be used as the last node for a network.
+        """
+        # Call the base class' constructor.
+        Node.__init__(self, [y, a])
+
+    def forward(self):
+        """
+        Calculates the mean squared error.
+        """
+        # NOTE: We reshape these to avoid possible matrix/vector broadcast
+        # errors.
+        #
+        # For example, if we subtract an array of shape (3,) from an array of shape
+        # (3,1) we get an array of shape(3,3) as the result when we want
+        # an array of shape (3,1) instead.
+        #
+        # Making both arrays (3,1) insures the result is (3,1) and does
+        # an elementwise subtraction as expected.
+        pred = self.inbound_nodes[0].value.reshape(-1, 1)
+        a = self.inbound_nodes[1].value.reshape(-1, 1)
+        self.value = np.sum(np.square(pred-a))/len(pred)
 
 """
 No need to change anything below here!
